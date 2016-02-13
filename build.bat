2e2e0760e8c7b7792423731ccd2f937b76cd3f30
Samuel Vincent Paal <samuel.v.paal@gmail.com>
2016-02-13 13:56:13 +0200

    Fixed VS solution. Added bitmap buffer to window.

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-mkdir build
-pushd build
-cl -Zi "..\src\main_win32.cpp" User32.lib Gdi32.lib
+mkdir dist || true
+pushd dist
+cl -Zi "..\code\main_win32.cpp" User32.lib Gdi32.lib
 popd
---
+++ code/main_win32.cpp
@@ -0,0 +1,161 @@
+#include <windows.h>
+
+#define internal static
+#define local_persist static
+#define global_variable static
+
+global_variable bool Running;
+
+global_variable BITMAPINFO BitmapInfo;
+global_variable HBITMAP BitmapHandle;
+global_variable void* BitmapMemory;
+global_variable HDC BitmapDeviceContext;
+
+internal void Win32ResizeDIBSection(int Width, int Height)
+{
+
+    if (BitmapHandle)
+    {
+        DeleteObject(BitmapHandle);
+    }
+    if (!BitmapDeviceContext)
+    {
+        BitmapDeviceContext = CreateCompatibleDC(0);
+    }
+
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = Width;
+    BitmapInfo.bmiHeader.biHeight = Height;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+    BitmapHandle = CreateDIBSection(
+        BitmapDeviceContext,
+        &BitmapInfo,
+        DIB_RGB_COLORS,
+        &BitmapMemory,
+        0,
+        0);
+}
+
+internal void Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+{
+    StretchDIBits(
+        DeviceContext, 
+        X, Y, Width, Height,
+        X, Y, Width, Height,
+        BitmapMemory,
+        &BitmapInfo,
+        DIB_RGB_COLORS,
+        SRCCOPY
+    );
+}
+
+
+LRESULT CALLBACK Win32MainWindowCallback(
+
+    HWND Window,
+    UINT Message,
+    WPARAM MessageParams1,
+    LPARAM MessageParams2)
+{
+
+    LRESULT Result = 0;
+    
+    switch (Message) {
+        
+    case WM_SIZE:
+    {
+        RECT ClientRect;
+        GetClientRect(Window, &ClientRect);
+        int Width = ClientRect.right - ClientRect.left;
+        int Height = ClientRect.bottom - ClientRect.top;
+        Win32ResizeDIBSection(Width, Height);
+    } break;
+
+    case WM_DESTROY:
+    {
+        Running = false;
+    } break;
+    
+    case WM_CLOSE:
+    {
+        Running = false;
+    } break;
+
+    case WM_ACTIVATEAPP:
+    {
+        OutputDebugStringA("WM_ACTIVATEAPP\n");
+    } break;
+
+    case WM_PAINT:
+    {
+        PAINTSTRUCT Paint;
+        HDC DeviceContext = BeginPaint(Window, &Paint);
+        int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+        int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+        int X = Paint.rcPaint.left;
+        int Y = Paint.rcPaint.top;
+        Win32UpdateWindow(BitmapDeviceContext, X, Y, Width, Height);
+        EndPaint(Window, &Paint);
+    } break;
+
+    default:
+    {
+        Result = DefWindowProc(Window, Message, MessageParams1, MessageParams2);
+    } break;
+    
+    }
+
+    return(Result);
+}
+
+int CALLBACK WinMain(
+    HINSTANCE Instance,
+    HINSTANCE PrevInstance,
+    LPSTR CommandLine,
+    int ShowCode)
+{
+
+    WNDCLASS WindowClass = {};
+    WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+    WindowClass.lpfnWndProc = Win32MainWindowCallback;
+    WindowClass.hInstance = Instance;
+    WindowClass.lpszClassName = "RootsWindowName";
+
+    if (RegisterClass(&WindowClass)) {
+        HWND WindowHandle = CreateWindowEx(
+            0,
+            WindowClass.lpszClassName,
+            "Roots",
+            WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+            CW_USEDEFAULT, CW_USEDEFAULT,
+            CW_USEDEFAULT, CW_USEDEFAULT,
+            0,
+            0,
+            Instance,
+            0
+            );
+        
+        if (WindowHandle) {
+            Running = true;
+            MSG Message;
+            while (Running) {
+                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+                if (MessageResult > 0) {
+                    TranslateMessage(&Message);
+                    DispatchMessage(&Message);
+                } else {
+                    break;
+                }
+            }
+        } else {
+            // TODO: error handling
+        }
+    } else {
+        // TODO: error handling
+    }
+    
+    return 0;
+}
