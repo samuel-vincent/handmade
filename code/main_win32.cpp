ce6ed28a42efc3835a3160c8f71d2728c2224dc3
Samuel Vincent Paal <samuel.v.paal@gmail.com>
2016-02-14 19:24:23 +0200

    .

--- code/main_win32.cpp
+++ code/main_win32.cpp
@@ -7,44 +7,46 @@
 global_variable bool Running;
 
 global_variable BITMAPINFO BitmapInfo;
-global_variable HBITMAP BitmapHandle;
 global_variable void* BitmapMemory;
-global_variable HDC BitmapDeviceContext;
+
+global_variable int BitmapWidth;
+global_variable int BitmapHeight;
 
 internal void Win32ResizeDIBSection(int Width, int Height)
 {
 
-    if (BitmapHandle)
+    if (BitmapMemory)
     {
-        DeleteObject(BitmapHandle);
-    }
-    if (!BitmapDeviceContext)
-    {
-        BitmapDeviceContext = CreateCompatibleDC(0);
+        VirtualFree(BitmapMemory, 0, MEM_RELEASE);
     }
 
+    BitmapWidth = Width;
+    BitmapHeight = Height;
+    
     BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
-    BitmapInfo.bmiHeader.biWidth = Width;
-    BitmapInfo.bmiHeader.biHeight = Height;
+    BitmapInfo.bmiHeader.biWidth = BitmapWidth;
+    BitmapInfo.bmiHeader.biHeight = BitmapHeight;
     BitmapInfo.bmiHeader.biPlanes = 1;
     BitmapInfo.bmiHeader.biBitCount = 32;
     BitmapInfo.bmiHeader.biCompression = BI_RGB;
 
-    BitmapHandle = CreateDIBSection(
-        BitmapDeviceContext,
-        &BitmapInfo,
-        DIB_RGB_COLORS,
-        &BitmapMemory,
-        0,
-        0);
+    int BytesPerPixel = 4;
+    int BitmapMemorySize = (Width * Height)*BytesPerPixel;
+    BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
 }
 
-internal void Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+internal void Win32UpdateWindow(HDC DeviceContext, RECT* WindowRect, int X, int Y, int Width, int Height)
 {
+
+    int WindowWidth = WindowRect->right - WindowRect->left;
+    int WindowHeight = WindowRect->bottom - WindowRect->top;
+    
     StretchDIBits(
         DeviceContext, 
-        X, Y, Width, Height,
-        X, Y, Width, Height,
+//        X, Y, Width, Height,
+//        X, Y, Width, Height,
+        0, 0, BitmapWidth, BitmapHeight, 
+        0, 0, WindowWidth, WindowHeight,
         BitmapMemory,
         &BitmapInfo,
         DIB_RGB_COLORS,
@@ -97,7 +99,7 @@ LRESULT CALLBACK Win32MainWindowCallback(
         int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
         int X = Paint.rcPaint.left;
         int Y = Paint.rcPaint.top;
-        Win32UpdateWindow(BitmapDeviceContext, X, Y, Width, Height);
+        Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
         EndPaint(Window, &Paint);
     } break;
 
